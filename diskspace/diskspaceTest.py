db053debe60f2a413873ebf2e7e5b3cce4ce85dd
@AmandaMuniz <amanda.muniiz@outlook.com>
2018-06-22 16:15:15 -0300

    Adicionando teste da função subprocess_check_output

--- diskspace/diskspaceTest.py
+++ diskspace/diskspaceTest.py
@@ -7,7 +7,13 @@ class DiskspaceTest(unittest.TestCase):
         blocks = 224
         result = "112.00Kb"
         command = bytes_to_readable(blocks)
-        self.assertEqual(command, result)    
+        self.assertEqual(command, result)
+
+    def test_subprocess_check_output(self):
+        command = 'du'
+        firstResult = subprocess.check_output(command.strip().split(' '))
+        finalResult = subprocess_check_output(command)
+        self.assertEqual(firstResult, finalResult)
 
 if __name__ == '__main__':
     unittest.main()
